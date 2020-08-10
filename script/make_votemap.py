351e8ada0aae5d773d3b5be69e641252e317f44f
Tina NKH <tina.nikoukhah@gmail.com>
2020-08-10 21:13:43 +0200

    votemap

--- script/make_votemap.py
+++ script/make_votemap.py
@@ -5,20 +5,24 @@ import sys
 import imageio
 import numpy as np
 
+import matplotlib.pyplot as plt
+cmap = plt.get_cmap('Accent')
+
 
 im = imageio.imread(sys.argv[1])
 height, width = im.shape
 
 output = np.zeros((height, width, 3))
 
-for x in range(width):
-    for y in range(height):
+for x in range(height):
+    for y in range(width):
         if (im[x,y] == 255):
-            output[x,y] = (0, 0, 0)
+            output[x,y] = np.array((0, 0, 0))
         else:
-            output[x,y] = (int(1 + 3*im[x,y]),
-                           int(1 + 2*im[x,y]),
-                           int(1+ im[x,y]))
-
+            rgb = 255*np.array(cmap((im[x,y]+1)/64)[:3])
+            output[x,y] = rgb.astype(np.uint8)
+            # output[x,y] = (int(1 + 3*im[x,y]),
+            #                int(1 + 2*im[x,y]),
+            #                int(1+ im[x,y]))
 
 imageio.imwrite('colored_votes.png', output)
